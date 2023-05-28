b387a6edab5bfdf63cfdbaf9aac3661d6cd47524
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-28 09:44:30 +0530

    Code Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
-import star from '../star.png'
+import React, { useState } from "react";
+import "../styles/App.css";
+import star from "../star.png";
 const App = () => {
-  return (
-    <div id="main">
-      <img src={star}  height="300px" width="300px" />
-    </div>
-  )
-}
+    const [count, setCount] = useState(300);
 
+    return (
+        <div id="main">
+            <img
+                src={star}
+                height={`${count}px`}
+                width={`${count}px`}
+                onClick={() => setCount(count + 2)}
+            />
+        </div>
+    );
+};
 
 export default App;
